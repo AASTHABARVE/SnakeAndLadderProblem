1e54769dd5733f3c2e278d9547c7ac700189d4f1
aastha <aasthabarve0@gmail.com>
2023-04-04 19:48:49 +0530

    added random 1 to 6 number

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLadder {
+
+    int position = 0;
+    static void dieRole(){
+
+        int dieValue = (int)(Math.random()*10%6)+1;
+        System.out.println(dieValue);
+    }
     public static void main(String args[]){
-        int position = 0;
+        dieRole();
+
+
+
+
     }
 }
