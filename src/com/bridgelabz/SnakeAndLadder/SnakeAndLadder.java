c49391016ac83092a2a13bcef0fcab90d93526cc
aastha <aasthabarve0@gmail.com>
2023-04-04 20:06:13 +0530

    check player position player play or not

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -5,14 +5,30 @@ public class SnakeAndLadder {
     int position = 0;
     static void dieRole(){
 
+
         int dieValue = (int)(Math.random()*10%6)+1;
         System.out.println(dieValue);
     }
+    static void play(){
+
+        int number = (int)(Math.random()*10%3);
+        switch (number){
+            case 0:
+                System.out.println("No move");
+                break;
+            case 1:
+                System.out.println(" move ahead");
+                break;
+            case 2:
+                System.out.println(" move behind");
+                break;
+
+
+        }
+    }
     public static void main(String args[]){
         dieRole();
-
-
-
+        play();
 
     }
 }
